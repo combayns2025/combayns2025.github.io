5f97e301bd00a718d36a6badc49c77cc3c1ae267
combayns2025 <combayns2025@proton.me>
2025-01-20 17:37:59 +0100

    Update and rename main.js to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-// Select the hamburger icon and the navbar menu
+// Toggle navigation menu on small screens
 const hamburger = document.querySelector('.hamburger');
 const navbarMenu = document.querySelector('.navbar-menu');
 
-// Toggle the menu when the hamburger is clicked
 hamburger.addEventListener('click', () => {
   navbarMenu.classList.toggle('show');
 });
